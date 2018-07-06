a282524624467900cdb47fb035d4e3738662a9b5
Mikolla <miqolay@gmail.com>
2018-07-06 18:23:17 +0500

    2. Реализовать класс Tracker

--- src/main/java/ru/bondarenko/start/StartUI.java
+++ src/main/java/ru/bondarenko/start/StartUI.java
@@ -2,15 +2,69 @@ package ru.bondarenko.start;
 
 import ru.bondarenko.models.*;
 
+//c:\projects\Tracker>java -cp target\tracker-1.0.jar ru.bondarenko.start.StartUI
+//c:\projects\Tracker>mvn clean package
 
 public class StartUI {	
 	public static void main(String[] args) {
 		Tracker tracker = new Tracker();
-		tracker.add(new Task("first task", "first desc"));
-	for (Item item : tracker.getAll()) {
-		System.out.println(item.getName());
+		tracker.add(new Task("first  task",  "first  desc"));
+		tracker.add(new Task("second task",  "second desc"));
+		tracker.add(new Task("third  task",  "third  desc"));
+		tracker.add(new Task("4th    task",  "4th    desc"));
+		tracker.add(new Task("5th    task",  "5th    desc"));
+		tracker.add(new Task("6th    task",  "6th    desc"));
+		tracker.add(new Task("7th    task",  "7th    desc"));
+		tracker.add(new Task("8th    task",  "8th    desc"));
+		tracker.add(new Task("9th    task",  "9th    desc"));
+		tracker.add(new Task("10th   task",  "10th   desc"));
+		tracker.add(new Task("11th   task",  "11th   desc"));
+		tracker.add(new Task("11th   task",  "11th   desc"));
+		tracker.add(new Task("11th   task",  "11th   desc"));
+		tracker.add(new Task("11th   task",  "11th   desc"));
+		tracker.add(new Task("11th   task",  "11th   desc"));
+	for (Item item : tracker.findAll()) {
+		System.out.println(item.getName() + " " + item.getId());
+	}
+	System.out.println("----------------------------------------");
+	
+	System.out.println("do fives task and out again");
+	
+	tracker.replace(tracker.findAll()[2].getId(),new Task("fives task", "fives desc"));
+	
+	for (Item item : tracker.findAll()) {
+		System.out.println(item.getName() + " " + item.getId());
+	}
+	System.out.println("----------------------------------------");
+	
+	System.out.println("deleting 6-th element and print out");	
+	String ID = tracker.findAll()[6].getId();
+	System.out.println("deleting 6-th ID = " + ID);
+	tracker.delete(ID);
+	for (Item item : tracker.findAll()) {
+		if(item!=null){
+		System.out.println(item.getName() + " " + item.getId());
+		}
+		else {
+			System.out.println("empty element");
+		}
+	}
+	
+	System.out.println("----------------------------------------");
+	
+	System.out.println("name finder and printing out");	
+	
+	for (Item item : tracker.findByName("11th   task")) {
+		if(item!=null){
+		System.out.println(item.getName() + " " + item.getId());
+		}
+		else {
+			System.out.println("empty element");
+		}
 	}
 	
 	}
+	
+	
 	
 }
--- src/main/java/ru/bondarenko/start/Tracker.java
+++ src/main/java/ru/bondarenko/start/Tracker.java
@@ -2,9 +2,10 @@ package ru.bondarenko.start;
 
 import ru.bondarenko.models.*;
 import java.util.*;
+import java.lang.*;
 
 public class Tracker {	
-	private Item[] items = new Item[10];
+	private Item[] items = new Item[15];
 	private int position = 0;
 	private static final Random RN = new Random();
 	public Item add(Item item) {
@@ -27,12 +28,49 @@ public class Tracker {
 		return String.valueOf(System.currentTimeMillis() + RN.nextInt());
 	}
 	
-	public Item[] getAll() {
+	public Item[] findAll() {
 		Item[] result = new Item[this.position];
 		for (int index = 0; index != position; index++ ) {
 			result[index] = this.items[index];
 		}
 		return result;
+	}
+	
+	public void replace(String id, Item item) {
+		for (int index = 0; index != position; index++ ) {
+			if (this.items[index].getId().equals(id)) {
+				item.setId(this.generateId());
+				this.items[index] = item;
+			}
+		}
+	}
+	
+	public void delete(String id) {
+		for (int index = 0; index != position; index++ ) {
+			if (this.items[index].getId().equals(id)) {
+				Item[] result = new Item[position-1];
+				System.arraycopy(this.items, 0, result, 0, index);
+				System.arraycopy(this.items, index+1, result, index, position - index-1);
+				items = result; 
+				position--;
+				break;
+			}
+		}
+	}
+	
+	
+	public Item[] findByName(String key) {
+		Item[] temp = new Item[position];
+		int tempindex = 0;
+		for (int index = 0; index != position; index++ ) {
+			if (this.items[index].getName().equals(key)) {
+				temp[tempindex] = this.items[index];
+				tempindex++;
+			}
+		}
+         Item[] result = new Item[tempindex];
+         System.arraycopy(temp, 0, result, 0, tempindex);		 
+	return result;
 	}
 	
 
